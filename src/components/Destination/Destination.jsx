d3dfdef393c38ec21fb24360ac44b34799ccd515
EwelinaManiecka <emaniecka3@gmail.com>
2023-12-20 17:44:27 +0100

    add destination component with img

--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -1,3 +1,8 @@
+import Wiena1 from "../assets/h4-wien-opera.jpg";
+import Wiena2 from "../assets/h9-wien-panoramic.jpg";
+
+
+
 const Destination = ( ) => {
     return (
         <div className="destintion">
@@ -7,6 +12,11 @@ const Destination = ( ) => {
                 <div className="textDescr">
                     <h2>Wiena, Austria</h2>
                     <p>Opis Wiednia po angielsku</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam blanditiis animi, voluptate nobis nisi corrupti suscipit quisquam cumque commodi nam aliquam porro incidunt tempora ratione. A nobis asperiores possimus placeat.</p>
+                </div>
+                <div className="image">
+                    <img alt="" src={Wiena1} />
+                    <img alt="" src={Wiena2} />
                 </div>
             </div>
         </div>
